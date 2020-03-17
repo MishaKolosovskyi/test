0d98da7f2530a3fd2a06bb6c5aee6d50d10809ea
MishaKolosovskyi <57897820+MishaKolosovskyi@users.noreply.github.com>
2020-03-17 15:14:57 +0200

    Delete ReviewsService.java

--- src/main/java/com/task/service/ReviewsService.java
+++
@@ -1,9 +0,0 @@
-package com.task.service;
-
-import java.util.List;
-
-public interface ReviewsService {
-    List<String> getMostActiveUsers();
-    List<Integer> getMostCommentedFood();
-    List<String> getMostUsedWords();
-}
