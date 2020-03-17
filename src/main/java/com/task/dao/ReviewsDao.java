b8f2d6470b55bed803dc9ce9436c562bb6c719fe
MishaKolosovskyi <57897820+MishaKolosovskyi@users.noreply.github.com>
2020-03-17 15:15:14 +0200

    Delete ReviewsDao.java

--- src/main/java/com/task/dao/ReviewsDao.java
+++
@@ -1,9 +0,0 @@
-package com.task.dao;
-
-import java.util.List;
-
-public interface ReviewsDao {
-    List<String> getMostActiveUsers();
-    List<Integer> getMostCommentedFood();
-    List<String> getMostUsedWords();
-}
